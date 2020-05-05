b9821d71d890b7ea322151a3d886d32fb6eb2c09
vinny-paris <vinny@iastate.edu>
2020-05-05 15:32:38 -0500

    fully operational

--- R/opt_rot_vec.R
+++ R/opt_rot_vec.R
@@ -53,7 +53,7 @@ opt_rot_vec <- function(design, return_n = 5){
   dets <- apply(hope, 1, tester_d, design = d, inv = inv)
   det_org <- tester_d(design = d, rotv = c(rep(0,f), FALSE), inv = inv)
                   
-  best_omegas <- omegas[order(omegas, decreasing = TRUE)][c(1:return_n),]
+  best_omegas <- omegas[order(omegas, decreasing = TRUE)][c(1:return_n)]
   
   
   finals <- cbind(best_omegas, dets, dets/det_org, unqieness, matrix(candidate_vecs[,-(f+1)], ncol = f))
