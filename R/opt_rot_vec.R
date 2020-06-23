ef5a41f8cf0af05f5b87e746f1f6bfbabd8cdb7c
vinny-paris <vinny@iastate.edu>
2020-06-23 14:25:02 -0500

    naming issue caused by removal of duplicates fixed

--- R/opt_rot_vec.R
+++ R/opt_rot_vec.R
@@ -77,7 +77,7 @@ opt_rot_vec <- function(design, return_n = 5, opt = "Omega"){
   if(opt == 'Omega') {finals <- arrange(as.data.frame(finals), desc('Omega value'), desc('Det'))[c(1:return_n),]}
   if(opt == 'Det') {finals <- arrange(as.data.frame(finals), desc('Det'))[c(1:return_n),]}
   if(opt == 'Run_Size') {finals <- arrange(as.data.frame(finals), 'Run Size', desc('Det'))[c(1:return_n),]}
-  if(opt == 'Min_Incident') {finals <- arrange(as.data.frame(finals), desc(inc_min[,1]), (inc_min[,2]), desc('Det'))[c(1:return_n),]}
+  if(opt == 'Min_Incident') {finals <- arrange(as.data.frame(finals), desc('Min. Incident'), ('Frequency'), desc('Det'))[c(1:return_n),]}
   
 
   return(finals)
